a47803c5f6940a1b5bafb7ead2366c3a62ff2cd7
Alexandre N Munoz-Oliveira <am52695@srcs.loc>
2017-05-01 13:53:16 -0700

    more btn changes, working on combat functions

--- create.js
+++ create.js
@@ -1,29 +1,96 @@
 function enemy(species) {
     this.species = species;
     this.focus=0
-    if (this.species = "dog.png")
-        this.focus = 2;
-}
+    if (this.species = "dog.png"){
+          this.focus = 2;
+    }
+
+       }
  function punch() {
      if (this.species = "dog.png") {
-         this.focus++;
+         this.focus=this.focus+1;
+         console.log(this.focus);
      }
-     this.display = function first() {
-         document.write("It got even angrier.")
      }
- }
+
 function kick() {
     if (this.species = "dog.png") {
-        this.focus++;
+        this.focus=this.focus+1;
+        console.log(this.focus);
     }
-    this.display1 = function second() {
-         document.write("It got even angrier.")
 }
 function pet() {
     if (this.species = "dog.png") {
-        this.focus--;
+        this.focus=this.focus-1;
+        console.log(this.focus);
     }
-    this.display3 = function third() {
-         document.write("It started to calm down.")
+
     }
+
+function btn1() {
+    document.getElementById('btn1').style.visibility = 'hidden';
+    document.getElementById('btn5').style.visibility = 'visible';
+    document.getElementById('btn2').style.visibility = 'hidden';
+    document.getElementById('btn3').style.visibility = 'visible';
+    document.getElementById('btn4').style.visibility = 'visible';
+    document.getElementById('backbtn1').style.visibility = 'visible';
+}
+
+function backbtn1() {
+    document.getElementById('btn1').style.visibility = 'visible';
+    document.getElementById('btn2').style.visibility = 'visible';
+    document.getElementById('backbtn1').style.visibility = 'hidden';
+    document.getElementById('texttohide').style.visibility = 'hidden';
+    document.getElementById('texttohide2').style.visibility = 'hidden';
+    document.getElementById('texttohide3').style.visibility = 'hidden';
+    document.getElementById('absolute-text1').style.visibility = 'visible';
+    document.getElementById('btn3').style.visibility = 'hidden';
+    document.getElementById('btn4').style.visibility = 'hidden';
+    document.getElementById('btn5').style.visibility = 'hidden';
+}
+
+function checkbtn() {
+    document.getElementById('btn2').style.visibility = 'hidden';
+    document.getElementById('texttohide').style.visibility = 'visible';
+    document.getElementById('absolute-text1').style.visibility = 'hidden';
+    document.getElementById('btn1').style.visibility = 'hidden';
+    document.getElementById('backbtn1').style.visibility = 'visible';
+}
+
+function btn3() {
+    document.getElementById('btn1').style.visibility = 'hidden';
+    document.getElementById('btn2').style.visibility = 'hidden';
+    document.getElementById('backbtn1').style.visibility = 'visible';
+    document.getElementById('texttohide').style.visibility = 'hidden';
+    document.getElementById('texttohide2').style.visibility = 'hidden';
+    document.getElementById('texttohide3').style.visibility = 'visible';
+    document.getElementById('absolute-text1').style.visibility = 'hidden';
+    document.getElementById('btn3').style.visibility = 'hidden';
+    document.getElementById('btn4').style.visibility = 'hidden';
+    document.getElementById('btn5').style.visibility = 'hidden';
+}
+
+function btn4() {
+    document.getElementById('btn1').style.visibility = 'hidden';
+    document.getElementById('btn2').style.visibility = 'hidden';
+    document.getElementById('backbtn1').style.visibility = 'visible';
+    document.getElementById('texttohide').style.visibility = 'hidden';
+    document.getElementById('texttohide2').style.visibility = 'visible';
+    document.getElementById('absolute-text1').style.visibility = 'hidden';
+    document.getElementById('btn3').style.visibility = 'hidden';
+    document.getElementById('btn4').style.visibility = 'hidden';
+    document.getElementById('btn5').style.visibility = 'hidden';
+}
+
+function btn5() {
+    document.getElementById('btn1').style.visibility = 'hidden';
+    document.getElementById('btn2').style.visibility = 'hidden';
+    document.getElementById('backbtn1').style.visibility = 'visible';
+    document.getElementById('texttohide').style.visibility = 'hidden';
+    document.getElementById('texttohide2').style.visibility = 'hidden';
+    document.getElementById('texttohide3').style.visibility = 'visible';
+    document.getElementById('absolute-text1').style.visibility = 'hidden';
+    document.getElementById('btn3').style.visibility = 'hidden';
+    document.getElementById('btn4').style.visibility = 'hidden';
+    document.getElementById('btn5').style.visibility = 'hidden';
 }
