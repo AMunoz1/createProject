10406854b192b85423f5b5bffebee97239f26845
rlara1 <ace100.rl@gmail.com>
2017-04-30 19:57:21 -0700

    Fixing the fight system

--- create.js
+++ create.js
@@ -1,2 +1,29 @@
-
-
+function enemy(species) {
+    this.species = species;
+    this.focus=0
+    if (this.species = "dog.png")
+        this.focus = 2;
+}
+ function punch() {
+     if (this.species = "dog.png") {
+         this.focus++;
+     }
+     this.display = function first() {
+         document.write("It got even angrier.")
+     }
+ }
+function kick() {
+    if (this.species = "dog.png") {
+        this.focus++;
+    }
+    this.display1 = function second() {
+         document.write("It got even angrier.")
+}
+function pet() {
+    if (this.species = "dog.png") {
+        this.focus--;
+    }
+    this.display3 = function third() {
+         document.write("It started to calm down.")
+    }
+}
